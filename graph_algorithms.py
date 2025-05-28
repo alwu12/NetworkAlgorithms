c4a576ff3d382aa3c4046a47087bc0621e13a671
alwu12 <114692112+alwu12@users.noreply.github.com>
2025-05-27 20:38:47 -0700

    added degeneracy to triangle counting

--- graph_algorithms.py
+++ graph_algorithms.py
@@ -5,6 +5,7 @@ from collections import deque, defaultdict
 from random import randint, seed
 
 
+
 def get_diameter(graph: Graph) -> int: #use a heuristic instead of getting exact diameter from bfsing all nodes
 	def bfs(g: Graph, source_node) -> int:
 		longest_path = 0
@@ -50,6 +51,64 @@ def get_diameter(graph: Graph) -> int: #use a heuristic instead of getting exact
 
 	return dmax
 
+
+def get_d_ordering(graph: Graph) -> tuple[list[int], list[list[int]]]:
+	n = graph.get_num_nodes()
+	L = []
+	dv = [len(graph.get_neighbors(v)) for v in range(n)]#gets the degree of each vertex
+	max_degree = max(dv)
+	D = [[] for _ in range(max_degree+1)] #list of lists that determine which nodes are of which degree
+	#for example: D = [[], [1, 2, 3], [4]] means 0 nodes of 0 degree, 3 of 1 degree and 1 of 2 degree
+	
+	for i in range(n):
+		D[dv[i]].append(i) #append current vertex to their degree list
+		#for example 1 would get put in the 2nd list because that would be it's degree in dv
+	
+	N_v = [[] for _ in range(n)] #this will track neighbors of v that come before it in the ordering.
+		
+	HL = {i:False for i in range(n)} #hash table to check whether a vertex is in L
+	k = 0 #used to track degeneracy
+	for _ in range(n):
+		#Let i be the smallest index such that D[i] is nonempty.
+
+		for i in range(len(D)):
+			if D[i]:#if the bucket is empty, find a bucket that isnt empty
+				v = D[i].pop() 
+				'''
+				Select a vertex v from D[i]. Add v to the beginning of L and remove it
+				from D[i]. Mark v as being in L (e.g., using a hash table, HL).
+				'''
+				k = max(k,i)
+				L.append(v)
+				HL[v] = True
+				for w in graph.get_neighbors(v):
+					if not HL[w]:
+						dv[w] -= 1
+						D[dv[w]+1].remove(w)
+						D[dv[w]].append(w)
+						N_v[v].append(w)
+				break
+
+	return L[::-1], N_v
+
+
+def get_triangles(graph: Graph) -> int:
+	triangle_count = 0
+	L, nv = get_d_ordering(graph)
+	for v in L:
+		neighbors_before_v = nv[v]
+
+		for i in range(len(neighbors_before_v)):
+			u = neighbors_before_v[i]
+			for j in range(len(neighbors_before_v)):
+				w = neighbors_before_v[j]
+				if w < u:
+					continue
+				if graph.is_adjacent(u,w):
+					triangle_count+=1
+	return triangle_count
+		
+'''
 def get_triangles(graph: Graph) -> int:
     triangle_count = 0
     num_nodes = graph.get_num_nodes()
@@ -70,6 +129,8 @@ def get_triangles(graph: Graph) -> int:
                 if graph.is_adjacent(v, w):
                     triangle_count += 1
     return triangle_count
+'''
+
 
 def get_clustering_coefficient(graph: Graph) -> float:
 	total_triplets = 0
